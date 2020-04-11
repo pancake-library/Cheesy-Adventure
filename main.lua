1d96afcc03c01fb237f26cfa26e2fce9ac02d83b
Mighty Pancake <MightyPancake@users.noreply.github.com>
2020-04-11 21:35:29 +0200

    next moves
    -Ship can't get out of borders
    -Asteroids exist

--- main.lua
+++ main.lua
@@ -2,7 +2,9 @@ pancake =  require "pancake"
 function love.load()
 	love.graphics.setBackgroundColor(0.1,0.1,0.1,1) --So it won't merge with pancake's background!!!
 	pancake.init({window = {pixelSize = love.graphics.getHeight()/96, width = 96, height = 96}}) --Initiating pancake and setting pixelSize, so that the pancake display will be the height of the window! pixelSize is how many pixels every pancake pixel should take
-	pancake.debugMode = true
+	--pancake.loadAnimation = nil
+	--pancake.paused = false
+	--pancake.debugMode = true
 	loadImages()
 	loadShipLevel()
 	pancake.background.image = pancake.images.background
@@ -24,18 +26,33 @@ function loadImages()
 	pancake.addImage("down_clicked", "images/ui")
 	--Adding background (it will be more detailed later xD)
 	pancake.addImage("background", "images")
+	--Adding asteroids
+	pancake.addImage("asteroid1", "images")
+	pancake.addImage("asteroid2", "images")
 end
 
 function loadShipLevel(level)--level is a number of the level. This function loads everything that is needed for the ship levels
 	pancake.objects = {}
 	pancake.timers = {}
 	pancake.physics.gravityY = 0
-	ship = pancake.applyPhysics(pancake.addObject({name = "ship", x = 64, y = 64, width = 14, height = 9, offsetX = -1, offsetY = -5}))
+	ship = pancake.applyPhysics(pancake.addObject({name = "ship", x = 2, y = 64, width = 14, height = 9, offsetX = -1, offsetY = -5}))
 	pancake.addAnimation("ship","idle","images/animations",180)
 	pancake.changeAnimation(ship,"idle")
 	levelType = "ship"-- This variable indicates what type of level are we in!
 	pancake.cameraFollow = ship
-	pancake.addObject()
+	ship.maxVelocity = 10
+	for w = 1, 10 do
+		for i = 1, 20 do
+			pancake.applyForce(createAsteroid(math.random(w*100,w*100+100), math.random(0,80), math.random(1,1)),{x=-math.random(0,7), y = math.random(0,7), relativeToMass = true},1)
+		end
+	end
+end
+
+function createAsteroid(x,y,number)
+	local ret
+	if number == 1 then
+		return pancake.applyPhysics(pancake.addObject({image = "asteroid1", name = "asteroid", x = x, y = y, width = 13, height = 13, offsetX = -2, offsetY = -1,layer = 2}))
+	end
 end
 
 function pancake.onCollision() --This function will be called whenever a physic object collides with a colliding object!
@@ -73,6 +90,17 @@ function love.update(dt)
 		end
 		if pancake.isButtonClicked(down) then
 			pancake.applyForce(ship, {y = 25, relativeToMass = true})
+		end
+		if ship.y < 2 then
+			ship.y = 2
+			ship.velocityY = 0
+		elseif ship.y > 86 then
+			ship.y = 86
+			ship.velocityY = 0
+		end
+		if ship.x < 0 then
+			ship.x = 0
+			ship.velocityX = 0
 		end
 	end
 end
